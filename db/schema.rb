b49739d2019be4122d74a45b2ecdff86d1212160
Almudena Garcia <murtra@murtra.net>
2014-03-16 19:12:40 +0100

    add category to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140316175942) do
+ActiveRecord::Schema.define(version: 20140316181142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20140316175942) do
     t.integer  "stock"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "category"
   end
 
 end
