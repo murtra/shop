ae24f327df5a07f60207ed0cfea1fe38f4ca0d9f
Almudena Garcia <murtra@murtra.net>
2014-03-23 15:48:30 +0100

    add nested attributes in order, with delete LineItem

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
+  before_action :load_products, only: [:new, :edit]
 
   # GET /orders
   # GET /orders.json
@@ -14,11 +15,12 @@ class OrdersController < ApplicationController
 
   # GET /orders/new
   def new
-    @order = Order.new
+    @order = Order.new  
+    @order.line_items.build
   end
 
   # GET /orders/1/edit
-  def edit
+  def edit 
   end
 
   # POST /orders
@@ -69,6 +71,10 @@ class OrdersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.require(:order).permit(:status, :payment_mode, :address, :city, :name, :last_name)
-    end
+      params.require(:order).permit(:status, :payment_mode, :address, :city, :name, :last_name, line_items_attributes: [:id, :quantity, :price, :product_id, :_destroy])
+    end 
+    
+    def load_products
+      @products = Product.all
+    end   
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
-class Order < ActiveRecord::Base
-
+class Order < ActiveRecord::Base 
   has_many :line_items
-  has_many :products, through: :line_items
-
+  has_many :products, through: :line_items 
+  
+  accepts_nested_attributes_for :line_items, allow_destroy: true
 end
