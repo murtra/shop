cb2fcdba58b3dc620c19ec45af9ecc88fb2afa5e
Almudena Garcia <murtra@murtra.net>
2014-03-23 10:51:41 +0100

    add price to Product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140322204512) do
+ActiveRecord::Schema.define(version: 20140323094423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20140322204512) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "category"
+    t.decimal  "price",        precision: 8, scale: 2
   end
 
 end
