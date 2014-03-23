764f559ba6ee5e2277e44fa37c0294aecfbb2e33
Almudena Garcia <murtra@murtra.net>
2014-03-23 21:09:45 +0100

    add reject_if to accepts_nested_attributes_for in Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,6 @@ class Order < ActiveRecord::Base
   has_many :line_items
   has_many :products, through: :line_items 
   
-  accepts_nested_attributes_for :line_items, allow_destroy: true
+  accepts_nested_attributes_for :line_items, allow_destroy: true,
+    reject_if: proc { |attributes| attributes[:product_id].blank? or attributes[:quantity].blank? }  
 end
