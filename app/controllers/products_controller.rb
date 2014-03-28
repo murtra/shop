ce7e2f3a59328525a8304a6fddd42b93674ecd07
Almudena Garcia <murtra@murtra.net>
2014-03-28 22:06:29 +0100

    add price in views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :stored_at, :manufacturer, :stock)
+      params.require(:product).permit(:name, :description, :stored_at, :manufacturer, :stock, :price)
     end
 end
