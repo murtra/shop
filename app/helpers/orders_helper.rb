610190ecae542e750fbac5494d676c30da26bda1
Almudena Garcia <murtra@murtra.net>
2014-04-22 12:51:26 +0200

    add select tag for payment modes

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,5 @@
 module OrdersHelper
+  def allowed_payment_modes_with_translations
+    Hash[I18n.t('order.allowed_payment_modes').zip(Order::ALLOWED_PAYMENT_MODES)]
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,33 +1,31 @@
-class Order < ActiveRecord::Base   
-  #ALLOWED_PAYMENT_MODES = I18n.t('order.allowed_payment_modes') # "cash", "card", "paypal"
-  ALLOWED_STATUS = I18n.t('order.allowed_status') # ["pending", "send", "received"]    
-  
-  before_validation :set_status, only: [:create]   
-  
+class Order < ActiveRecord::Base
+  ALLOWED_PAYMENT_MODES = ["cash", "card", "paypal"] #I18n.t('order.allowed_payment_modes')
+  ALLOWED_STATUS = ["pending", "send", "received"]
+
+  before_validation :set_status, only: [:create]
+
   validates :name, :last_name, :address, :city, :payment_mode, :status, presence: true
-  validates :payment_mode, inclusion: { in: I18n.t('order.allowed_payment_modes'), message: I18n.t('errors.messages.inclusion') }
-  #validates :status, inclusion: { in: Order::ALLOWED_STATUS, message: I18n.t('generic.invalid') } 
-    
-  #validates_associated :line_items
-  
+  validates :payment_mode, inclusion: { in: Order::ALLOWED_PAYMENT_MODES, message: I18n.t('errors.messages.inclusion') }
+  #validates :status, inclusion: { in: Order::ALLOWED_STATUS, message: I18n.t('generic.invalid') }
+
   has_many :line_items
-  has_many :products, through: :line_items 
-  
+  has_many :products, through: :line_items
+
   accepts_nested_attributes_for :line_items, allow_destroy: true,
-    reject_if: proc { |attributes| attributes[:product_id].blank? || attributes[:quantity].blank? }  
-  
+    reject_if: proc { |attributes| attributes[:product_id].blank? || attributes[:quantity].blank? }
+
   def total_price
     line_items.to_a.sum {|l| l.price * l.quantity}
   end
-    
-  private 
-  
+
+  private
+
     # Set status to "pending" on create
-    def set_status   
+    def set_status
       self.status = "pending"
-    end 
-    
+    end
+
     def allowed_payment_modes
       I18n.t('order.allowed_payment_modes')
-    end 
+    end
 end
